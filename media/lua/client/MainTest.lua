d08f7b63fb918be283b6d19e4b436f736f551703
Dianagarcia33 <41555299+Dianagarcia33@users.noreply.github.com>
2023-04-08 01:15:15 -0500

    Diana

--- media/lua/client/MainTest.lua
+++ media/lua/client/MainTest.lua
@@ -1,18 +1,24 @@
 require "Chat/ISChat"
 require "pl.pretty"
 
-local ChatSystem = require("ChatSystem");
+-- local ChatSystem = require("ChatSystem");
 
-local function testDeath()
-    print("YEEAH!!! zombie died");
-    ChatSystem.addLineToChat("YEEAH!!! zombie died", "<RGB:1,1,1>");
-end
+-- local function testDeath()
+--     print("YEEAH!!! zombie died");
+--     ChatSystem.addLineToChat("YEEAH!!! zombie died", "<RGB:1,1,1>");
+-- end
 
-local function onMessage(chatMessage, tabId)
-    print("LOG : PKC: onMessage=" .. chatMessage:getText());
+-- local function onMessage(chatMessage, tabId)
+--     print("LOG : PKC: onMessage=" .. chatMessage:getText());
+--     print("LOG : PKC: RawMessage=" .. dumb(chatMessage));
+-- end
+
+local function OnPlayerDeath(player)
     print("LOG : PKC: RawMessage=" .. dumb(chatMessage));
 end
 
-Events.OnZombieDead.Add(testDeath);
-Events.OnAddMessage.Add(onMessage);
+Events.OnPlayerDeath.Add(OnPlayerDeath)
+
+-- Events.OnZombieDead.Add(testDeath);
+-- Events.OnAddMessage.Add(onMessage);
 
